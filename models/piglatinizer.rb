0cd75aebd28b45b9d9ee87edef557baa99e3723a
Dani Schuhman <dani8439@aol.com>
2018-08-23 19:47:45 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,5 +1,23 @@
 class PigLatinizer
 
+  attr_reader :text
+  
+    def piglatinize(text)
+        text.split.map do |word|
+          if /\A[aeiou]/i.match(word)
+            "#{word}way"
+          else
+            cons = word.split(/[aeiou]/).first
+            "#{word.sub(cons,"")}#{cons}ay"
+          end
+          end.join(' ')
+        end
+  
+      def to_pig_latin(text)
+        piglatinize(text)
+      end
+    end
+
 
   # def piglatinize(text)
   #   vowels = %[a e i o u A E I O U]
@@ -19,20 +37,4 @@ class PigLatinizer
 end
 
 # Short Version:
-# attr_reader :text
-#
-#   def piglatinize(text)
-#       text.split.map do |word|
-#         if /\A[aeiou]/i.match(word)
-#           "#{word}way"
-#         else
-#           cons = word.split(/[aeiou]/).first
-#           "#{word.sub(cons,"")}#{cons}ay"
-#         end
-#         end.join(' ')
-#       end
-#
-#     def to_pig_latin(text)
-#       piglatinize(text)
-#     end
-#   end
+
