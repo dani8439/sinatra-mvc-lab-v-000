aec0a1bc97216cd673e874041f9d80cc82eaca46
Dani Schuhman <dani8439@aol.com>
2018-08-23 18:18:46 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,6 +5,14 @@ class PigLatinizer
   end
 
   def piglatinize(word)
+    if !consonant?(word[0])
+      word = word + "w"
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3)
+    elsif consonant?(word[0] && consonant?(word[1]))
+      word = word.slice(2..-1) + word.slice(0,2)
+    else
+      word = word.slice(1..-1) + word.slice(0)
   end
 
   # need to split the sentence
