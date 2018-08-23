4064875fdff5e73236c8bb99eeec464a6e9545e4
Dani Schuhman <dani8439@aol.com>
2018-08-23 18:06:30 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,12 +1,4 @@
 class PigLatinizer
-  attr_reader :text
-
-  VOWELS = %w[aeiouAEIOU]
-
-  def initialize(text)
-    @text = text
-  end
-
   def to_pig_latin(text)
     text.split.collect{|word| piglatinize(word)}.join(" ")
   end 
