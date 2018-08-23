9c49c2e7121dce0d813494baed62b1535806730e
Dani Schuhman <dani8439@aol.com>
2018-08-23 18:05:33 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,6 +9,11 @@ class PigLatinizer
 
   def to_pig_latin(text)
     text.split.collect{|word| piglatinize(word)}.join(" ")
+  end 
+
+  def piglaitinze(word)
+    return word << "way" if VOWELS.include?(word[0])
+    word = word.split("")
 
 
 end
