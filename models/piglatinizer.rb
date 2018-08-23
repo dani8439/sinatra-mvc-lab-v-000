95382e0844be1ea005d57fc34c043d2b1b7d5202
Dani Schuhman <dani8439@aol.com>
2018-08-23 18:34:02 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -26,6 +26,16 @@ class PigLatinizer
 
 end
 
+def piglatinize(word)
+  if word =~ (/\A[aeiou]/i)
+    word = word + 'ay'
+  elsif word =~ (/\A[^aeiou]/i)
+    match = /\A[^aeiou]/i.match(word)
+    word = match.post_match + match.to_s + 'ay'
+  end
+  word
+end
+
 
 # def piglatinize(word)
 #   if VOWELS.include?(word[0]) == true
