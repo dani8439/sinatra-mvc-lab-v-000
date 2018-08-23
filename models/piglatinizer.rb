16fb05d6fff1ac00828a60a97404613907cac192
Dani Schuhman <dani8439@aol.com>
2018-08-23 18:06:35 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,15 +1,15 @@
 class PigLatinizer
   def to_pig_latin(text)
     text.split.collect{|word| piglatinize(word)}.join(" ")
-  end 
+  end
 
   def piglaitinze(word)
     return word << "way" if VOWELS.include?(word[0])
     word = word.split("")
 
     until VOWELS.include?(word[0])
-      letter = word.shift 
-      word = word << letter 
+      letter = word.shift
+      word = word << letter
     end
 
     word.join + "ay"
