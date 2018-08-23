dc7558cbd4c61f09973de77183e4aa6d2ae20295
Dani Schuhman <dani8439@aol.com>
2018-08-23 18:40:31 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,21 +1,24 @@
 class PigLatinizer
 
-  attr_reader :text
 
-    def piglatinize(text)
-        text.split.map do |word|
-          #regex match 
-          if /\A[aeiou]/i.match(word)
-            "#{word}way"
-          else
-            cons = word.split(/[aeiou]/).first
-            "#{word.sub(cons,"")}#{cons}ay"
-          end
-          end.join(' ')
+end
+
+
+
+attr_reader :text
+
+  def piglatinize(text)
+      text.split.map do |word|
+        if /\A[aeiou]/i.match(word)
+          "#{word}way"
+        else
+          cons = word.split(/[aeiou]/).first
+          "#{word.sub(cons,"")}#{cons}ay"
         end
-
-      def to_pig_latin(text)
-        piglatinize(text)
+        end.join(' ')
       end
 
+    def to_pig_latin(text)
+      piglatinize(text)
+    end
 end
