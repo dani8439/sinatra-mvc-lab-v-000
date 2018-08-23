d58029f6010c32c0d48a5cee697499b2ef90fb4d
Dani Schuhman <dani8439@aol.com>
2018-08-23 18:43:19 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,27 +1,7 @@
 class PigLatinizer
 
   
-  def piglatinize(name)
-  if name[0].downcase.match(/[aeiou]/)
-    name = name + "way"
-  else
-    until name.split('')[0].downcase.match(/[aeiou]/)
-      name = name.split('').rotate(1).join
-    end
-    name = name.split('').join + "ay"
-  end
-  name
 end
-
-def to_pig_latin(phrase)
-  phrases = Array.new
-  phrase.split(' ').each do |name|
-    phrases << piglatinize(name)
-  end
-  phrases.join(' ')
-end
-
-
 
 
 # Short Version:
