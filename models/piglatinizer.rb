b8e120434d96bcbbabaf41e614bbf736e79b7ef3
Dani Schuhman <dani8439@aol.com>
2018-08-23 18:19:18 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,26 +1,24 @@
 class PigLatinizer
-  def consonant?(char)
-    !char.match(/[aAeEiIoOuU]/)
-    #scans a regext to see if character stards wtih a vowel or consonant -- instead of setting VOWELS = %w[a e i o u A E I O U]
-  end
-
-  def piglatinize(word)
+   def piglatinize(word)
+     #words that start with a vowel
     if !consonant?(word[0])
       word = word + "w"
+      #words start with 3 consonants
     elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
       word = word.slice(3..-1) + word.slice(0,3)
-    elsif consonant?(word[0] && consonant?(word[1]))
+      #word starst with 2 consonants
+    elsif consonant?(word[0]) && consonant?(word[1])
       word = word.slice(2..-1) + word.slice(0,2)
+      #word starts with 1 consonant
     else
       word = word.slice(1..-1) + word.slice(0)
-    end 
+    end
     word << "ay"
   end
-
-  # need to split the sentence
-  def to_pig_latin(phrase)
-    # splits the sentence, then collect -- iterate over each word - piglatinize each word - then rejoin the collected words back into a sentence
-    phrase.split.collect{|word| piglatinize(word)}.join(" ")
+   def consonant?(char)
+    !char.match(/[aAeEiIoOuU]/)
   end
-
-end
+   def to_pig_latin(sentence)
+    sentence.split.collect { |word| piglatinize(word) }.join(" ")
+  end
+end 
