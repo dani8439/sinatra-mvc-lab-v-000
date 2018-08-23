7b5715779e1d352487f5aa6f5564c6daf9d2766e
Dani Schuhman <dani8439@aol.com>
2018-08-23 19:47:30 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,21 +1,21 @@
 class PigLatinizer
 
 
-  def piglatinize(text)
-    vowels = %[a e i o u A E I O U]
-    if text.start_with?(vowels)
-      "#{text}way"
-    else
-      cons = text.split(/[aeiou]/).first
-      #finds where the word needs to be split.
-       "#{text.sub(cons,"")}#{cons}ay"
-       #substitutes the beginning of word(cons) with blank, then tacks consonant + latinzed bit on end
-    end
-  end
-
-  def to_pig_latin(phrase)
-    phrase.split.collect{|word| piglatinize(word) }.join(" ")
-  end
+  # def piglatinize(text)
+  #   vowels = %[a e i o u A E I O U]
+  #   if text.start_with?(vowels)
+  #     "#{text}way"
+  #   else
+  #     cons = text.split(/[aeiou]/).first
+  #     #finds where the word needs to be split.
+  #      "#{text.sub(cons,"")}#{cons}ay"
+  #      #substitutes the beginning of word(cons) with blank, then tacks consonant + latinzed bit on end
+  #   end
+  # end
+  # 
+  # def to_pig_latin(phrase)
+  #   phrase.split.collect{|word| piglatinize(word) }.join(" ")
+  # end
 end
 
 # Short Version:
