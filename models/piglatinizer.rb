ec3ae33b29b2fe79539ad4b1eb421929d1ec9b9d
Dani Schuhman <dani8439@aol.com>
2018-08-23 18:46:26 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,16 +1,21 @@
 class PigLatinizer
 
   def piglatinize(word)
-    if word[0].downcase.match(/[aeiou]/)
-      word = word + "way"
-    else
-      until word.split('')[0].downcase.match(/[aeiou]/)
-        word = word.split('').rotate(1).join
-      end
-      word = word.split('').join + "ay"
-    end
-    word
+  if !consonant?(word[0])
+    word = word + "w"
+  elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+    word = word.slice(3..-1) + word.slice(0,3)
+  elsif consonant?(word[0]) && consonant?(word[1])
+    word = word.slice(2..-1) + word.slice(0,2)
+  else
+    word = word.slice(1..-1) + word.slice(0)
   end
+  word << "ay"
+end
+
+def consonant?(char)
+  !char.match(/[aAeEiIoOuU]/)
+end
 
   def to_pig_latin(phrase)
    phrase.split.collect { |word| piglatinize(word) }.join(" ")
