41841e75089d5a389f556229336ab17f4b46056e
Dani Schuhman <dani8439@aol.com>
2018-08-23 19:51:13 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -34,4 +34,27 @@ class PigLatinizer
   #   phrase.split.collect{|word| piglatinize(word) }.join(" ")
   # end
 
-# Short Version:
+  # class PigLatinizer
+  #   # Create a constant array of vowels
+  #   VOWELS = %w[a e i o u A E I O U]
+  # 
+  # 
+  #   # need to split the sentence
+  #   def to_pig_latin(phrase)
+  #     # splits the sentence, then collect -- iterate over each word - piglatinize each word - then rejoin the collected words back into a sentence
+  #     phrase.split.collect{|word| piglatinize(word)}.join(" ")
+  #   end
+  # 
+  #   def piglatinize(word)
+  #     return word << "way" if VOWELS.include?(word[0])
+  #     word = word.split("")
+  # 
+  #     until VOWELS.include?(word[0])
+  #       letter = word.shift
+  #       word = word << letter
+  #     end
+  # 
+  #     word.join + "ay"
+  #   end
+  # 
+  # end
