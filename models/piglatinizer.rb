0b3bee8978330a97da771e5d9eba35181bf55acb
Dani Schuhman <dani8439@aol.com>
2018-08-23 18:16:54 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,14 +1,15 @@
 class PigLatinizer
 
 
+  def consonant?(char)
+    !char.match(/[aAeEiIoOuU]/)
+    #scans a regext to see if character stards wtih a vowel or consonant -- instead of setting VOWELS = %w[a e i o u A E I O U]
+  end
+
   # need to split the sentence
   def to_pig_latin(phrase)
     # splits the sentence, then collect -- iterate over each word - piglatinize each word - then rejoin the collected words back into a sentence
     phrase.split.collect{|word| piglatinize(word)}.join(" ")
-  end
-
-  def consonant?(char)
-    !char.match(/[aAeEiIoOuU]/)
   end
 
 end
